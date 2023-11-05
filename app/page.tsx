900e2d76ac07326e625df945b36b76ce1511e42d
okadasorachi <Soraoka0418@outlook.jp>
2023-11-06 02:02:06 +0900

    add form

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'DashBoard',
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
